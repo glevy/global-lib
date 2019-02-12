ecfd64f0ad5c7989e424a3fe324397ac10bce75c
Gilad Levy <ynot.gilad@gmail.com>
2019-02-12 10:28:41 +0200

    set defaults

--- vars/downloadArtifact.groovy
+++ vars/downloadArtifact.groovy
@@ -2,12 +2,11 @@
 
 import java.util.Map
 
-def call(Map args){
-
+def call(Map args = [artifactId:"${JOB_BASE_NAME}",artifactVersion:"${BUILD_NUMBER}",targetPath:"${JOB_BASE_NAME}"]){
 
 	String artifactId = args.artifactId ?: "${JOB_BASE_NAME}"
 	String artifactVersion = args.artifactVersion ?: "${BUILD_NUMBER}"
-	String targetPath = args.targetPath ?: artifactId
+	String targetPath = args.targetPath ?: "${artifactId}"
 
 		try{
 			rtServer (
@@ -34,13 +33,13 @@ def call(Map args){
 
         
 			
-		writeFile file: "${workspace}\\download\\pom.xml", text: libraryResource("download/pom.xml")
+		writeFile file: "${workspace}/download/pom.xml", text: libraryResource("download/pom.xml")
 		
 
 		rtMavenRun (
 			// Tool name from Jenkins configuration.
 			tool: 'MAVEN_TOOL',
-			pom: "${workspace}\\download\\pom.xml",
+			pom: "${workspace}/download/pom.xml",
 			goals: " -B -U dependency:unpack -Ddownload.artifactId='${artifactId}' -Ddownload.version='${artifactVersion}' -Ddownload.outDir='${targetPath}' -DoverWriteReleases=true",
 			// Maven options.
 			opts: "-Xms1024m -Xmx4096m",
